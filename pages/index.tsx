ccce018f26271a8d790239e5b91442ad0b6b671a
ellagoris <eduardofidera@gmail.com>
2022-09-03 04:26:55 -0300

    feat: list collections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
 /** @jsxImportSource theme-ui */
 import Head from "next/head"
 
-import { Heading, Text, Label } from "@theme-ui/components"
+import { Heading, Text, Label, Flex } from "@theme-ui/components"
 import { useCallback, useEffect, useState } from "react"
 import { LoadingIcon } from "@/components/icons/LoadingIcon"
 
+type PopularCollectionsApiResponse = {
+  collectionSymbol: string
+  name: string
+  image: string
+  ownerCount: number
+  tokenCount: number
+  /** Total volume in SOL */
+  totalVol: number
+  /** Volume in SOL for the period */
+  vol: number
+  volDelta: number
+  txns: number
+  /** Floor Price in LAMPORT */
+  fp: number
+  rank: number
+  updatedAt: number
+}[]
+
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false)
-  const [collections, setCollections] = useState(null)
+  const [collections, setCollections] =
+    useState<PopularCollectionsApiResponse>(null)
 
   const fetchCollections = useCallback(async () => {
     try {
@@ -16,7 +35,7 @@ export default function Home() {
         "https://stats-mainnet.magiceden.io/collection_stats/popular_collections/sol?limit=1000&window=7d"
       )
 
-      const parsed = await raw.json()
+      const parsed: PopularCollectionsApiResponse = await raw.json()
       setCollections(parsed)
       console.log(parsed)
     } catch (e) {
@@ -49,7 +68,45 @@ export default function Home() {
         </Heading>
 
         {isLoading && <LoadingIcon />}
-        <Text>Found {collections && collections.length} collections </Text>
+
+        <Text
+          sx={{
+            flexDirection: "column",
+          }}
+        >
+          <Heading variant="heading2">Popular collections</Heading>
+          <Text>Found {collections && collections.length} collections: </Text>
+
+          {collections &&
+            collections.map(({ collectionSymbol, name, tokenCount, image }) => {
+              return (
+                <Flex
+                  sx={{
+                    alignItems: "center",
+                    gap: "1.6rem",
+                    padding: ".8rem",
+                    borderBottom: "1px solid",
+                    borderColor: "primary",
+                    borderRadius: ".4rem",
+                  }}
+                  key={collectionSymbol}
+                >
+                  <img
+                    sx={{
+                      maxWidth: "4rem",
+                    }}
+                    onError={(e) => {
+                      e.currentTarget.src =
+                        "https://via.placeholder.com/320x320"
+                    }}
+                    src={image}
+                  />
+                  <Text>{name}</Text>
+                  <Text>{tokenCount}</Text>
+                </Flex>
+              )
+            })}
+        </Text>
       </main>
     </>
   )
