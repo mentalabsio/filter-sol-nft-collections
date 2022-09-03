44f1f755cb6bb097de1cd44881c05f2f52191f0e
ellagoris <eduardofidera@gmail.com>
2022-09-03 04:15:32 -0300

    feat: init fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,34 @@
 import Head from "next/head"
 
 import { Heading, Text, Label } from "@theme-ui/components"
+import { useCallback, useEffect, useState } from "react"
+import { LoadingIcon } from "@/components/icons/LoadingIcon"
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [collections, setCollections] = useState(null)
+
+  const fetchCollections = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      const raw = await fetch(
+        "https://stats-mainnet.magiceden.io/collection_stats/popular_collections/sol?limit=1000&window=7d"
+      )
+
+      const parsed = await raw.json()
+      setCollections(parsed)
+      console.log(parsed)
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchCollections()
+  }, [fetchCollections])
+
   return (
     <>
       <main
@@ -15,11 +41,15 @@ export default function Home() {
           maxWidth: "64rem",
           margin: "0 auto",
           marginTop: "4rem",
+          gap: "3.2rem",
         }}
       >
         <Heading mb=".8rem" variant="heading1">
-          Themed App
+          Filter Solana NFT Collections
         </Heading>
+
+        {isLoading && <LoadingIcon />}
+        <Text>Found {collections && collections.length} collections </Text>
       </main>
     </>
   )
